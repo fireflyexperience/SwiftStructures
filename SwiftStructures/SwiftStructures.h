9ff4475659fad751c3d56fadb0b0316c423ea7d4
Andy Bennett <andybennett@Andys-MacBook-Pro.local>
2015-09-13 18:02:44 +0100

    change app target to be called Example add new module target

---
+++ SwiftStructures/SwiftStructures.h
@@ -0,0 +1,19 @@
+//
+//  SwiftStructures.h
+//  SwiftStructures
+//
+//  Created by Andy Bennett on 13/09/2015.
+//  Copyright © 2015 Arbutus Software Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftStructures.
+FOUNDATION_EXPORT double SwiftStructuresVersionNumber;
+
+//! Project version string for SwiftStructures.
+FOUNDATION_EXPORT const unsigned char SwiftStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftStructures/PublicHeader.h>
+
+
